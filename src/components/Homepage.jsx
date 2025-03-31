49cbc1401d86564c5d067f526d5a5398cbc914c7
Mehari Fitihamlak <meharifitih@gmail.com>
2025-03-31 22:02:30 +0300

    finsh home page layout

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,56 @@
-import {Carousel} from './'
+import { Carousel, HomePageCards, CarouselCategory, CarouselProduct } from "./";
 
 const Homepage = () => {
   return (
     <div className="bg-amazonclone-background">
-      <div className="min-w-[1000px] max-w-[1500px] m-auto bg-purple-600">
+      <div className="min-w-[1000px] max-w-[1500px] m-auto">
         Testing
-        <Carousel/>
+        <Carousel />
+        <div className="grid grid-cols-3 xl:grid-cols-4 -mt-80">
+          <HomePageCards
+            title={"Hello world"}
+            image={"../images/home_grid_1.jpg"}
+            link={"see terms and conditions"}
+          />
+          <HomePageCards
+            title={"Hello world"}
+            image={"../images/home_grid_2.jpg"}
+            link={"see terms and conditions"}
+          />
+          <HomePageCards
+            title={"Hello world"}
+            image={"../images/home_grid_3.jpg"}
+            link={"see terms and conditions"}
+          />
+          <HomePageCards
+            title={"Hello world"}
+            image={"../images/home_grid_4.jpg"}
+            link={"see terms and conditions"}
+          />
+          <HomePageCards
+            title={"Hello world"}
+            image={"../images/home_grid_5.jpg"}
+            link={"see terms and conditions"}
+          />
+          <HomePageCards
+            title={"Hello world"}
+            image={"../images/home_grid_6.jpg"}
+            link={"see terms and conditions"}
+          />
+          <HomePageCards
+            title={"Hello world"}
+            image={"../images/home_grid_7.jpg"}
+            link={"see terms and conditions"}
+          />
+          <div className="m-3 pt-8">
+            <img className="xl:hidden" src={"../images/banner_image_2.jpg"} />
+          </div>
+        </div>
+        <CarouselProduct />
+        <CarouselCategory />
+        <div className="h-[200px]">
+          <img className="h-[100%] m-auto" src={`../images/banner_image.jpg`} />
+        </div>
       </div>
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
-export {default as Homepage} from './Homepage'
-export {default as Carousel} from './Carousel'
+export { default as Homepage } from "./Homepage";
+export { default as Carousel } from "./Carousel";
+export { default as HomePageCards } from "./HomePageCards";
+export { default as CarouselCategory } from "./CarouselCategory";
+export { default as CarouselProduct } from "./CarouselProduct";
