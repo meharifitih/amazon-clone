2cde8f9384491dfb41b0a4779469d8aa27b2bd7b
Mehari Fitihamlak <meharifitih@gmail.com>
2025-04-01 16:12:27 +0300

    add navbar feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import {Homepage  } from './components'
+import {Homepage, NavBar  } from './components'
 
 const App = () => {
   return (
     <div >
+    <NavBar/>
       <Routes>
         <Route exact path='/' element={<Homepage/>} />
       </Routes>
